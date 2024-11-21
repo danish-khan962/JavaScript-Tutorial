94fdfd9b77262364b1b15c646b751e0fb3934f88
Danish Khan <dk831675@gmail.com>
2024-11-21 20:56:21 +0530

    JavaScript-Tutorial

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -13,3 +13,31 @@ if (true) {
 console.log("Outer variable var1: ", var1) //10
 console.log("Outer variable var2: ", var2) //20
 console.log("Outer variable var3: ", var3) //300 (because var do not follow scope)
+
+
+//Scope level and closure
+function one(){
+    const name = "Danish"
+    function two(){
+        const website = "github"
+        console.log(name)
+    }
+    // console.log(webiste) //this will give error because it is out of scope level
+    two()
+}
+one()
+
+
+/*++++++++++++++++++++++ IMPORTANT ++++++++++++++++++++++++*/
+//HOISTING
+
+console.log(addOne(5)) //calling before function , but it will run
+function addOne(num1){
+    return num1 + 1
+}
+
+//this expression can be known as hoisting, when we hold a function inside a variable
+console.log(addTwo(5)) // If we call before initialization , we encounter error
+const addTwo = function(num2){
+    return num2 + 1
+}
